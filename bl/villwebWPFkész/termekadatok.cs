a3f898bc079fb40e7605db3c8d80a67ac2ba2ec2
READYonHub <104925582+READYonHub@users.noreply.github.com>
2023-12-04 14:19:45 +0100

    Add files via upload

---
+++ bl/villwebWPFkész/termekadatok.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adatbaziskezeloWPF
+{
+    internal class termekadatok
+    {
+        public string kategorianev { get; set; }
+        public string termeknev { get; set; }
+        public string feszultseg { get; set; }
+        public int teljesitmeny { get; set; }
+        public string foglalat { get; set; }
+        public int elettartam { get; set; }
+        public int ar { get; set; }
+    }
+}
